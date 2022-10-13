3211cb45173bd5b1eb87688750365222c9ec6ba6
Шумайлова Нина <ninashum@gmail.com>
2022-10-13 22:12:29 +0300

    задача 1

---
+++ part1/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Программа, принимает целое число и выдает его квадрат*/ 
+
+// запрос числа от пользователя
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Число равно" +" " + number);
+
+// расчкт квадрата
+
+int sqrNumber = number * number;
+
+// формула, но применяется к вещ.цифрам (надо выбрать double, а не int)
+sqrNumber1 = Math.Pow (number, 2)
+
+//вывод результата
+Console.WriteLine($"Квадрат числа = {sqrNumber}");
